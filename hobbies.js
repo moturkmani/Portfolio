199207f943d98244895e49e47bd65a66be03e802
Mojtaba "Mo" Turkmani <110569341+moturkmani@users.noreply.github.com>
2025-03-25 16:59:42 -0600

    Update hobbies.js

--- hobbies.js
+++ hobbies.js
@@ -14,23 +14,23 @@ images.forEach((image, index) => {
     image.addEventListener('click', () => {
         currentImageIndex = index;
         expandedImage.src = image.src;
-        modal.style.display = "flex";
+        modal.style.display = "flex"; // Show the modal
     });
 });
 
 // Close the modal when clicking the close button
 closeModal.addEventListener('click', () => {
-    modal.style.display = "none";
+    modal.style.display = "none"; // Hide the modal
 });
 
 // Navigate to the previous image
 prevImage.addEventListener('click', () => {
     currentImageIndex = (currentImageIndex === 0) ? images.length - 1 : currentImageIndex - 1;
-    expandedImage.src = images[currentImageIndex].src;
+    expandedImage.src = images[currentImageIndex].src; // Change the source of the expanded image
 });
 
 // Navigate to the next image
 nextImage.addEventListener('click', () => {
     currentImageIndex = (currentImageIndex === images.length - 1) ? 0 : currentImageIndex + 1;
-    expandedImage.src = images[currentImageIndex].src;
+    expandedImage.src = images[currentImageIndex].src; // Change the source of the expanded image
 });
