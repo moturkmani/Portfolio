63a4f01d0a64d5503ccffb554ef2580ae95b4f42
Mojtaba "Mo" Turkmani <110569341+moturkmani@users.noreply.github.com>
2025-03-26 08:00:24 -0600

    Update projects.js

--- projects.js
+++ projects.js
@@ -4,6 +4,7 @@ const projectTitles = document.querySelectorAll('.project-title');
 // Add click event listener to each project title
 projectTitles.forEach((title) => {
     title.addEventListener('click', () => {
+        console.log('Title clicked'); // Check if this prints to the console
         const projectId = title.getAttribute('data-project'); // Get the corresponding project ID
         const projectDetails = document.getElementById(projectId); // Get the details container
 
